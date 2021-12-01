88965c6390fcf5c5e0dd0b144d93217fe71e1e1f
Genghis Philip <gphilip@nexben.com>
2021-12-01 10:53:40 -0600

    makes windows configurable using more list slicing

--- Day1/1-2.py
+++ Day1/1-2.py
@@ -1,3 +1,4 @@
 input = [int(x.strip()) for x in open('./input1-1.txt')]
-chunks_list = [input[x]+input[x+1]+input[x+2] for x in range(len(input[:-2:])) if x < len(input)-1]
+windowsize = 3
+chunks_list = [sum(input[x:x+windowsize]) for x in range(len(input[:-2:])) if x < len(input)-1]
 print(len([v for i, v in enumerate(chunks_list) if v > chunks_list[i-1] and i !=0]))
