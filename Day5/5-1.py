4826839c0219529e1dc73e5a8321e6d446944265
Genghis Philip <gphilip@nexben.com>
2021-12-05 13:01:51 -0600

    cleans up a bit of reuse

--- Day5/5-1.py
+++ Day5/5-1.py
@@ -20,32 +20,24 @@ grid = []
 for i in range(ymax+1):
     grid.append(list(gridline))
 
-
-def x_line(y1,y2,xcoord):
-    if y1 > y2:
-        high = y1+1
-        low = y2
+def line(a1,a2,coord,axis):
+    if a1 > a2:
+        high = a1+1
+        low = a2
     else:
-        high = y2+1
-        low = y1
+        high = a2+1
+        low = a1
     for i in range(low,high):
-        grid[i][xcoord] += 1
-
-def y_line(x1,x2,ycoord):
-    if x1 > x2:
-        high = x1+1
-        low = x2
-    else:
-        high = x2+1
-        low = x1
-    for i in range(low,high):
-        grid[ycoord][i] += 1
+        if axis == 'x':
+            grid[i][coord] += 1
+        if axis == 'y':
+            grid[coord][i] += 1
 
 for i in inputfile:
     if i[0][0] == i[1][0]:
-        x_line(i[0][1],i[1][1],i[0][0])
+        line(i[0][1],i[1][1],i[0][0],'x')
     elif i[0][1] == i[1][1]:
-        y_line(i[0][0],i[1][0],i[0][1])
+        line(i[0][0],i[1][0],i[0][1],'y')
 
 answer_total = 0
 
