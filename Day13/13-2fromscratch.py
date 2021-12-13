1a2b3343c01a521df88498ee862468aba8d011c6
Genghis Philip <gphilip@nexben.com>
2021-12-13 16:28:39 -0600

    13-2fromscratch is a less Hulk Smash solution

--- Day13/13-2fromscratch.py
+++ Day13/13-2fromscratch.py
@@ -1,23 +1,45 @@
-#abandoned this, but working on it made me realize I was setting my maxy/maxx based on DOTS, not based on PAPER
-
+#I did this after hours of struggling with the other way (13-2.py). Once I got that to work, I came back to this and it took like 20 minutes, because I am NOT a smart man.
 gridbasics = [x.strip() for x in open('input13.txt')]
 folds = ['x=655','y=447','x=327','y=223','x=163','y=111','x=81','y=55','x=40','y=27','y=13','y=6']
 
 occupied = set()
-maxY = 0
-maxX = 0
+maxY = 1310
+maxX = 894
+minY = 6
+minX = 40
 
 for i in gridbasics:
     a,b = i.split(',')
-    if int(a) > maxX:
-        maxX = int(a)
-    if int(b) > maxY:
-        maxY = int(b)
     occupied.add((int(a),int(b)))
 
-print(occupied)
 for fold in folds:
+    tempset = occupied.copy()
     axis = fold.split('=')[0]
     number = fold.split('=')[1]
-    for tup in occupied:
-        pass
+    number = int(number)
+    if axis == 'y': 
+        for tup in tempset:
+            if tup[1] > number:
+                newnum = number*2-tup[1]
+                newtup = (tup[0],newnum)
+                occupied.add(newtup)
+                occupied.remove(tup)
+    elif axis == 'x': 
+        for tup in tempset:
+            if tup[0] > number:
+                newnum = number*2-tup[0]
+                newtup = (newnum,tup[1])
+                occupied.add(newtup)
+                occupied.remove(tup)
+    
+grid = []
+for num in range(minY):
+    grid.append([' ' for x in range(minX)])
+
+for tup in occupied:
+    grid[tup[1]][tup[0]] = '#'
+
+with open('output.txt', 'a') as f:
+    for i in grid:
+        f.write(''.join(i)+'\n')
+    f.write('\n\n')
